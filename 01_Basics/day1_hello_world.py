c39176af0a3ae5da21f68e30ac8b3e977498b67e
Kavirsawant1108 <kavirsawant@gmail.com>
2026-02-28 14:02:42 +0530

    Added Day 1 - Hello World

---
+++ 01_Basics/day1_hello_world.py
@@ -0,0 +1,12 @@
+"""
+Day 1 - Hello World
+Author: Kabir Sawant
+Description: This program prints a welcome message.
+"""
+
+def main():
+    print("Hello, World!")
+    print("Welcome to my Daily Python Coding Journey 🚀")
+
+if __name__ == "__main__":
+    main()
