90a91f846669229dc0d8197b0c396c0f53e7ce66
Kavirsawant1108 <kavirsawant@gmail.com>
2026-03-01 11:23:51 +0530

    Added Day 3 - If Else Basic Programs

---
+++ 02_Conditionals/day3_if_else.py
@@ -0,0 +1,11 @@
+"""
+Day 3 - Program 1
+Check whether a number is Even or Odd
+"""
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
